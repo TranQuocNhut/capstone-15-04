6d9769224b4ccc48e36887f4786c6be63df3b524
BinTran04 <tranquocnhut2004@gmail.com>
2026-04-17 11:10:01 +0700

    fix: database ssl connection

--- src/configs/prisma.config.ts
+++ src/configs/prisma.config.ts
@@ -3,7 +3,12 @@ import pg from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 const adapter = new PrismaPg(pool)
 
 // Sử dụng Singleton pattern để đảm bảo chỉ có một instance Prisma Client duy nhất
