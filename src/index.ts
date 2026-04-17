3d4fe0daec824b76b09345761e784a90a5f0ec01
BinTran04 <tranquocnhut2004@gmail.com>
2026-04-17 11:18:15 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express, { Request, Response } from 'express'
-import prisma from './configs/prisma.config.js'
 import authRoutes from './routes/auth.route.js'
 import imageRoutes from './routes/image.route.js'
 import commentRoutes from './routes/comment.route.js'
@@ -22,14 +21,8 @@ app.use('/users', userRoutes)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 // Get all users
-app.get('/', async (req: Request, res: Response) => {
-  try {
-    const users = await prisma.user.findMany()
-    res.json(users)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal server error' })
-  }
+app.get('/', (req: Request, res: Response) => {
+  res.redirect('/api-docs')
 })
 
 // Sửa PORT để lấy từ môi trường của Railway
